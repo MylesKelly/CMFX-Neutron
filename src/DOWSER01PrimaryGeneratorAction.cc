2dae5fdfacdf2310ec2770985f43f4a3215b5eb0
jlball <jlball813@gmail.com>
2021-08-27 09:22:56 -0400

    Comments/cleanup in PrimaryGeneratorAction file

--- src/DOWSER01PrimaryGeneratorAction.cc
+++ src/DOWSER01PrimaryGeneratorAction.cc
@@ -51,88 +51,16 @@ DOWSER01PrimaryGeneratorAction::DOWSER01PrimaryGeneratorAction()
 : G4VUserPrimaryGeneratorAction(),
 fParticleGun(0)
 {
+  //Instantiate particle gun object:
   G4int nofParticles = 1;
   fParticleGun = new G4ParticleGun(nofParticles);
-  // nAngle = 0.;
-  G4double Emax = 6; // 1 MeV;
-  G4double Emin = -0.39794; // 0.4 eV;
-  G4double nEnergyT(0);
-  fnArray = 1295;
-  fAngleArray = 180;
-  G4double fEnergyT = 0;
+  
+  //Useful value for later:
   pi = 3.14159265358979323846264338328;
-  // Moon radius: 1737 km, LRO altitude: orbitH km
-  orbitH = 50.0; // ground (km)
-  moonR = 1737.0;
-  RoverRH = (moonR/(moonR + orbitH));
-  // vLRO = sqrt(G * moon_Mass /((orbitH+1737)*1000)); // LEO velocity in Y direction
-  vLRO = sqrt(4902808346033.4/((orbitH+moonR)*1000)); //
-  nEqvEnergy = 0.5*nMass*vLRO*vLRO/1.6022E-19; // eV
-  // neutron potential difference
-  // del U = G * n_Mass * moon_Mass * (1 / R_moon - 1 / (R_moon + H))
-  nPotDiff = 51252.715*(1./1737000 - 1./(1737000 + orbitH*1000)); // potential difference: U_orbit - U_surf
-  nMass = 1.6749E-27; // kg
-  moonMass = 7.34767309E+22; // kg
-  thetaCr = asin(RoverRH);
-  // nMC2 = nMass * 299790000.0 * 299790000.0; // joule
-  
-  std::ifstream theData("LNeutronIntSpectrum.DAT", std::ios::in);
-  G4cout << "Open Neutron Spectrum Data " << G4endl;
-  
-  if(!theData)
-  {
-    theData.close();
-    G4cout << "LNeutronIntSpectrum.DAT does not exit! " << G4endl;
-    G4double dElog10 = (Emax - Emin)/fnArray;
-    for (G4int copyNo=0; copyNo<fnArray; copyNo++)
-    {
-      nEnergy[copyNo]=pow(10,Emin + copyNo * dElog10);
-      intSpectrum[copyNo] = pow(nEnergy[copyNo],-0.89)
-      *(pow(10,Emin + (copyNo+0.5) * dElog10)-pow(10,Emin + (copyNo-0.5) * dElog10));
-      nEnergyT += intSpectrum[copyNo];
-      intSpectrum[copyNo] = nEnergyT;
-    }
-    intSpectrum[0] = 0;
-    for (G4int copyNo=0; copyNo<fnArray; copyNo++)
-    {
-      intSpectrum[copyNo] /=  nEnergyT;
-    }
-    
-  } else
-  {
-    for (G4int copyNo=0; copyNo<fnArray; copyNo++)
-    {
-      if (!theData.eof())
-      {
-        theData >> nEnergy[copyNo] >> intSpectrum[copyNo]  ;
-      }
-    }
-  }
-  theData.close();
-  
-  // Lunar neutron angular distribution, LEND's View
-  fEnergyT = 0;
-  for (G4int copyNo = 0; copyNo < fAngleArray; copyNo++)
-  {
-    // nFluxHEE[copyNo] = cos(0.5*copyNo*pi/fAngleArray)*std::sqrt(cos(0.5*copyNo*pi/fAngleArray))*sin(0.5*copyNo*pi/fAngleArray);
-    nFluxHEE[copyNo] = std::pow(cos(0.5*copyNo*pi/fAngleArray), 3./2.)*sin(0.5*copyNo*pi/fAngleArray);
-    nThetaHEE[copyNo] = copyNo*90./fAngleArray;
-    fEnergyT += nFluxHEE[copyNo];
-    nFluxHEE[copyNo] = fEnergyT;
-  }
-  
-  for (G4int copyNo = 0; copyNo < fAngleArray; copyNo++)
-  {
-    nFluxHEE[copyNo] /=  fEnergyT;
-  }
-  
-  
-  G4ParticleDefinition* particleDefinition
-  = G4ParticleTable::GetParticleTable()->FindParticle("neutron");
-  fParticleGun->SetParticleDefinition(particleDefinition);
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
-  fParticleGun->SetParticleEnergy(0.001032720*eV);
 
+  //Specify type of particle to be launched from the gun:
+  G4ParticleDefinition* particleDefinition = G4ParticleTable::GetParticleTable()->FindParticle("neutron");
+  fParticleGun->SetParticleDefinition(particleDefinition);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -151,24 +79,7 @@ void DOWSER01PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   z1(0), Al_z(0), Xe_z(0), sourceRadius(0), HDPE_z(0), spread(0), spread_angle(0);
   G4int copyNo(0), iflag(0);
   G4double xx(0), yy(0), zz(0), pPhiH(0), pVerH(0), xDiff(0), yDiff(0), zDiff(0);
-  // G4double xx(0), yy(0), zz(0), pPhiH(0), pVerH(0);
-  // In order to avoid dependence of PrimaryGeneratorAction
-  // on DetectorConstruction class we get world volume
-  // from G4LogicalVolumeStore
-  //
-  G4double worldZHalfLength = 0;
-  G4LogicalVolume* worlLV = G4LogicalVolumeStore::GetInstance()->GetVolume("World");
-  G4Box* worldBox = 0;
-  if ( worlLV) worldBox = dynamic_cast< G4Box*>(worlLV->GetSolid());
-  if ( worldBox ) {
-    worldZHalfLength = worldBox->GetZHalfLength();
-  }
-  else  {
-    G4cerr << "World volume of box not found." << G4endl;
-    G4cerr << "Perhaps you have changed geometry." << G4endl;
-    G4cerr << "The gun will be place in the center." << G4endl;
-  }
-  
+
   // begining of particle iteration - energy, position, angle assessment
   
   // NEW - SET EVENT INFORMATION
@@ -187,12 +98,8 @@ void DOWSER01PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   analysisManager->FillH1(1, std::log10(Eneutron));
   info->SetEnergyN(Eneutron);
   
-  //
-  Al_z = 0.8 *mm;
-  Xe_z = 7 *mm;
-  HDPE_z = 40*mm;
 
-  sourceRadius = 60*cm;  //+ Xe_z/2 + Al_z ;
+  sourceRadius = 60*cm;  
 
   //Randomly generate rotation angle
   rotationAngle = pi * G4UniformRand() + pi/2;
